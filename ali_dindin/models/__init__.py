c763adcc59fcb41647ff16a0f1845b023891584b
ongood <lxd@ongood.cn>
2019-07-08 14:40:54 +0800

    删除token

--- ali_dindin/models/__init__.py
+++ ali_dindin/models/__init__.py
@@ -2,7 +2,6 @@
 
 from . import systemc_conf
 from . import res_config_settings
-from . import dingding_token
 from . import department
 from . import employee
 from . import partner
--- ali_dindin/models/res_config_settings.py
+++ ali_dindin/models/res_config_settings.py
@@ -99,7 +99,6 @@ class ResConfigSettings(models.TransientModel):
             din_corpId=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_corpId'),
             din_appkey=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_appkey'),
             din_appsecret=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_appsecret'),
-            din_token=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_token'),
             din_delete_extcontact=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_delete_extcontact'),
             din_create_employee=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_create_employee'),
             din_update_employee=self.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_update_employee'),
@@ -119,7 +118,6 @@ class ResConfigSettings(models.TransientModel):
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_corpId', self.din_corpId)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_appkey', self.din_appkey)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_appsecret', self.din_appsecret)
-        self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_token', self.din_token)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_delete_extcontact', self.din_delete_extcontact)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_create_employee', self.din_create_employee)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_update_employee', self.din_update_employee)
@@ -130,26 +128,5 @@ class ResConfigSettings(models.TransientModel):
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_unsynchronized_hidden_departments', self.din_unsynchronized_hidden_departments)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_login_appid', self.din_login_appid)
         self.env['ir.config_parameter'].sudo().set_param('ali_dindin.din_login_appsecret', self.din_login_appsecret)
-        data = {
-            'name': '钉钉-定时更新token值',
-            'active': True,
-            'model_id': self.env['ir.model'].sudo().search([('model', '=', 'ali.dindin.get.token')]).id,
-            'state': 'code',
-            'user_id': self.env.user.id,
-            'numbercall': -1,
-            'interval_number': 90,
-            'interval_type': 'minutes',
-            'code': "env['ali.dindin.get.token'].get_token()",
-        }
-        if self.din_token:
-            cron = self.env['ir.cron'].sudo().search([('name', '=', "钉钉-定时更新token值")])
-            if len(cron) >= 1:
-                cron.sudo().write(data)
-            else:
-                self.env['ir.cron'].sudo().create(data)
-        else:
-            cron = self.env['ir.cron'].sudo().search(
-                [('code', '=', "env['ali.dindin.get.token'].get_token()")])
-            if len(cron) >= 1:
-                cron.sudo().unlink()
+
 
