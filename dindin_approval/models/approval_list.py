5b5122ff516d5e6e908bac893bb3c171ec9044c5
banqu <banqu@163.com>
2019-07-11 23:14:46 +0800

    暂存

--- dindin_approval/models/approval_list.py
+++ dindin_approval/models/approval_list.py
@@ -10,7 +10,7 @@ from odoo.addons.ali_dindin.models.dingtalk_client import get_client
 _logger = logging.getLogger(__name__)
 
 
-class DinDinApprovalTemplate(models.Model):
+class DingTalkApprovalList(models.Model):
     _name = 'dindin.approval.list'
     _description = "审批实例列表"
     _rec_name = 'name'
@@ -22,43 +22,42 @@ class DinDinApprovalTemplate(models.Model):
                                  string=u'公司', default=lambda self: self.env.user.company_id.id)
 
  
-    # TODO 需要进一步完善获取单个实例后写入到odoo对应的单据中
     @api.model
-    def get_processinstance(self, pid, pcode):
+    def get_processinstance(self, pid):
         """
         获取单个审批实例详情
-        :param pid:
-        :param pcode:
+
+        :param process_instance_id: 审批实例id
         :return:
         """
-        try:
-            client = get_client(self)
-            result = client.bpms.processinstance_get(pid)
-            logging.info(">>>获取审批实例详情返回结果{}".format(result))
-            if result.get('errcode') == 0:
-                process_instance = result.get('process_instance')
-                temp = self.env['dindin.approval.template'].sudo().search([('process_code', '=', pcode)])
-                if temp:
-                    appro = self.env['dindin.approval.control'].sudo().search([('template_id', '=', temp[0].id)])
-                    if appro:
-                        oa_model = self.env[appro.oa_model_id.model].sudo().search([('process_instance_id', '=', pid)])
-                        if not oa_model:
-                            # 获取发起人
-                            emp = self.env['hr.employee'].sudo().search([('din_id', '=', process_instance.get("originator_userid"))])
-                            data = {
-                                'title': process_instance.get('title'),
-                                'create_date': process_instance.get("create_time"),
-                                'originator_user_id': emp.id if emp else False,
-                            }
-                            # 审批状态
-                            if process_instance.get("status") == 'NEW':
-                                data.update({'oa_state': '00'})
-                            elif process_instance.get("status") == 'RUNNING':
-                                data.update({'oa_state': '01'})
-                            else:
-                                data.update({'oa_state': '02'})
+        # try:
+        #     client = get_client(self)
+        #     result = client.bpms.processinstance_get(pid)
+        #     logging.info(">>>获取审批实例详情返回结果{}".format(result))
+        #     if result.get('errcode') == 0:
+        #         process_instance = result.get('process_instance')
+        #         temp = self.env['dindin.approval.template'].sudo().search([('process_code', '=', pcode)])
+        #         if temp:
+        #             appro = self.env['dindin.approval.control'].sudo().search([('template_id', '=', temp[0].id)])
+        #             if appro:
+        #                 oa_model = self.env[appro.oa_model_id.model].sudo().search([('process_instance_id', '=', pid)])
+        #                 if not oa_model:
+        #                     # 获取发起人
+        #                     emp = self.env['hr.employee'].sudo().search([('din_id', '=', process_instance.get("originator_userid"))])
+        #                     data = {
+        #                         'title': process_instance.get('title'),
+        #                         'create_date': process_instance.get("create_time"),
+        #                         'originator_user_id': emp.id if emp else False,
+        #                     }
+        #                     # 审批状态
+        #                     if process_instance.get("status") == 'NEW':
+        #                         data.update({'oa_state': '00'})
+        #                     elif process_instance.get("status") == 'RUNNING':
+        #                         data.update({'oa_state': '01'})
+        #                     else:
+        #                         data.update({'oa_state': '02'})
 
-            else:
-                logging.info('>>>获取单个审批实例-失败，原因为:{}'.format(result.get('errmsg')))
-        except Exception as e:
-            raise UserError(e)
+        #     else:
+        #         logging.info('>>>获取单个审批实例-失败，原因为:{}'.format(result.get('errmsg')))
+        # except Exception as e:
+        #     raise UserError(e)
