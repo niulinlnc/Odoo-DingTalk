09b5be51a1b32c70cbc301dedebd4842265e8a03
ongood <lxd@ongood.cn>
2019-07-20 13:01:28 +0800

    改为新验证方式

--- auth_oauth_dingtalk/controllers/main.py
+++ auth_oauth_dingtalk/controllers/main.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
+import base64
+import hashlib
+import hmac
 import json
 import logging
-import urllib
+import time
+from urllib.parse import quote
 
 import requests
 import werkzeug.urls
 import werkzeug.utils
+from requests import ReadTimeout
 from werkzeug.exceptions import BadRequest
-import pycompat
 
-from odoo import http, tools
+from odoo import http
+from odoo.addons.ali_dindin.dingtalk.main import client
 from odoo.addons.auth_oauth.controllers.main import \
     OAuthController as Controller
 from odoo.addons.auth_oauth.controllers.main import OAuthLogin as Home
 from odoo.addons.web.controllers.main import ensure_db
 from odoo.exceptions import AccessDenied
 from odoo.http import request
-from odoo.addons.ali_dindin.dingtalk.main import client
-from dingtalk.client import SecretClient
-from odoo.exceptions import UserError
+from odoo.tools import pycompat
 
 _logger = logging.getLogger(__name__)
 
@@ -80,10 +83,8 @@ class OAuthController(Controller):
     def signin(self, provider_id, **kw):
 
         code = kw.get('code', "")
-        token = self.get_token()  # 不同于H5微应用获取的access_token
-        openid, persistent_code = self.get_persistent_code(code, token)
-        sns_token = self.get_sns_token(token, openid, persistent_code)
-        userinfo = self.get_userinfo(sns_token)
+        _logger.info("获得的code: %s", code)
+        userinfo = self.get_userid_by_unionid(code)
         userid = client.user.get_userid_by_unionid(userinfo['unionid']).get('userid')
         try:
             _logger.info("track...........")
@@ -117,143 +118,43 @@ class OAuthController(Controller):
             _logger.exception("OAuth2: %s", str(e))
             url = "/web/login?oauth_error=2"
 
-    def get_token(self):
+    def get_userid_by_unionid(self, code):
         """
-        获取钉钉移动应用的ACCESS_TOKEN
-        文档地址：https://open-doc.dingtalk.com/docs/api.htm?apiId=37175
-
-        :param appid: 由钉钉开放平台提供给开放应用的唯一标识
-        :param appsecret: 由钉钉开放平台提供的密钥
+        根据返回的临时授权码获取用户信息
+        :param code:
+        :return:
         """
-        params = request.env['ir.config_parameter'].sudo()
-
-        appid = params.get_param('ali_dindin.din_login_appid', default='')
-        appscret = params.get_param('ali_dindin.din_login_appsecret', default='')
-        if not appid or not appscret:
-            raise werkzeug.exceptions.NotFound()
-
-        params = dict(
-            appid=appid,
-            appsecret=appscret
-        )
-        url = "https://oapi.dingtalk.com/sns/gettoken"
-
-        link = "%s?%s" % (url, werkzeug.url_encode(params))
-        urlRequest = urllib.request.Request(link)
-        urlResponse = urllib.request.urlopen(urlRequest).read()
-        result = json.loads(str(urlResponse, encoding='utf-8'))
-        if result['errcode'] == 0:
-            return result['access_token']
-        else:
-            raise werkzeug.exceptions.BadRequest(result['errmsg'])
-
-    def get_persistent_code(self, tmp_auth_code, token):
-        """
-        获取用户授权的持久授权码
-        文档地址：https://open-doc.dingtalk.com/docs/api.htm?apiId=37161
-
-        :param tmp_auth_code: 用户授权给钉钉开放应用的临时授权码
-        """
-        params = dict(access_token=token)
-        body = {"tmp_auth_code": tmp_auth_code}
-        url = 'https://oapi.dingtalk.com/sns/get_persistent_code'
-        link = "%s?%s" % (url, werkzeug.url_encode(params))
-
-        result = requests.post(link, json=body, ).text
-        result = json.loads(result)
-        if result['errcode'] == 0:
-            return result['openid'], result['persistent_code']
-        else:
-            raise werkzeug.exceptions.BadRequest(result['errmsg'])
-
-    def get_sns_token(self, token, openid, persistent_code):
-        """
-        获取用户授权的SNS_TOKEN
-        文档地址：https://open-doc.dingtalk.com/docs/api.htm?apiId=37148
-
-        :param persistent_code: 用户授权给钉钉开放应用的持久授权码
-        :param openid: 用户的openid
-        """
-        params = dict(access_token=token)
-        body = {
-            "openid": openid,
-            "persistent_code": persistent_code
+        url = request.env['ali.dindin.system.conf'].sudo().search(
+            [('key', '=', 'getuserinfo_bycode')]).value
+        login_appid = request.env['ir.config_parameter'].sudo(
+        ).get_param('ali_dindin.din_login_appid')
+        key = request.env['ir.config_parameter'].sudo(
+        ).get_param('ali_dindin.din_login_appsecret')
+        # def current_milli_time(): return int(round(time.time() * 1000))
+        # msg = str(current_milli_time())
+        msg = pycompat.to_text(int(time.time() * 1000))
+        _logger.info("时间戳:%s", msg)
+        # ------------------------
+        # 签名
+        # ------------------------
+        signature = hmac.new(key.encode('utf-8'), msg.encode('utf-8'),
+                             hashlib.sha256).digest()
+        signature = quote(base64.b64encode(signature), 'utf-8')
+        data = {
+            'tmp_auth_code': code
         }
-        url = 'https://oapi.dingtalk.com/sns/get_sns_token'
-        link = "%s?%s" % (url, werkzeug.url_encode(params))
+        headers = {'Content-Type': 'application/json'}
+        new_url = "{}signature={}&timestamp={}&accessKey={}".format(
+            url, signature, msg, login_appid)
+        _logger.info("new_url:%s", new_url)
+        try:
+            result = requests.post(
+                url=new_url, headers=headers, data=json.dumps(data), timeout=15)
+            result = json.loads(result.text)
+            logging.info(">>>钉钉登录获取用户信息返回结果%s", result)
+            if result.get('errcode') == 0:
+                return result.get('user_info')
+            raise BadRequest(result)
 
-        result = requests.post(link, json=body, ).text
-        result = json.loads(result)
-        if result['errcode'] == 0:
-            return result['sns_token']
-        else:
-            raise werkzeug.exceptions.BadRequest(result['errmsg'])
-
-    def get_userinfo(self, sns_token):
-        """
-        获取用户授权的个人信息
-        文档地址：https://open-doc.dingtalk.com/docs/api.htm?apiId=37160
-        :param sns_token: 用户授权给开放应用的token
-        """
-        params = dict(sns_token=sns_token)
-
-        url = 'https://oapi.dingtalk.com/sns/getuserinfo'
-
-        result = requests.get(url, params).text
-        result = json.loads(result)
-        if result['errcode'] == 0:
-            return result['user_info']
-        else:
-            raise werkzeug.exceptions.BadRequest(result['errmsg'])
-
-    # def get_persistent_code(self, code):
-
-    #     try:
-    #         result = client.tbdingding.dingtalk_oapi_sns_get_persistent_code(tmp_auth_code=code)
-    #         logging.info('>>>:获取persistent_code返回结果%s', result)
-    #         return result['openid'], result['persistent_code']
-    #     except Exception as e:
-    #         raise UserError(e)
-
-    # def get_sns_token(self, openid, persistent_code):
-
-    #     try:
-    #         result = client.tbdingding.dingtalk_oapi_sns_get_sns_token(persistent_code=persistent_code, openid=openid)
-    #         logging.info('>>>:获取sns_token返回结果%s', result)
-    #         return result['sns_token']
-    #     except Exception as e:
-    #         raise UserError(e)
-
-    # def get_userinfo(self, sns_token):
-
-    #     try:
-    #         result = client.tbdingding.dingtalk_oapi_sns_getuserinfo(sns_token=sns_token)
-    #         logging.info('>>>:获取user_info返回结果%s', result)
-    #         return result['user_info']
-    #     except Exception as e:
-    #         raise UserError(e)
-
-    # def get_userid_by_unionid(self, access_token, unionid):
-    #     """
-    #     根据unionid获取成员的userid
-
-    #     :param unionid: 用户在当前钉钉开放平台账号范围内的唯一标识
-    #     :return:
-    #     """
-    #     try:
-    #         result = client.user.get_userid_by_unionid(unionid)
-    #         logging.info(">>>根据unionid获取成员的userid返回结果:%s", result)
-    #         return result
-    #     except Exception as e:
-    #         raise UserError(e)
-
-    # def get_access_token(self):
-
-    #     corp_id = tools.config.get('din_corpid', '').strip()
-    #     corp_secret = tools.config.get('din_corp_secret', '').strip()
-    #     try:
-    #         result = SecretClient(corp_id, corp_secret)
-    #         logging.info('>>>:获取user_info返回结果%s', result)
-    #         return result['access_token']
-    #     except Exception as e:
-    #         raise UserError(e)
+        except ReadTimeout:
+            return {'state': False, 'msg': '网络连接超时'}
