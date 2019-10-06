36b2b1d226f0bb652d10f6b971d9950ab418e313
banqu <banqu@163.com>
2019-10-06 21:58:43 +0800

    fix Copyright

--- og_attendance_count/static/src/js/hr_attendance_record.js
+++ og_attendance_count/static/src/js/hr_attendance_record.js
@@ -1,5 +1,5 @@
 /**
- *    Copyright (C) 2019 SuXueFeng
+ *    Copyright (C) 2019 OnGood
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU Affero General Public License as
