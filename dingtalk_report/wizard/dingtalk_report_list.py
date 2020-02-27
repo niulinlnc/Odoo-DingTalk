5c08d057b875644d0043661c5dc1b3133802005c
ongood <lxd@ongood.cn>
2020-02-27 14:45:27 +0800

    已存在日志，执行更新

--- dingtalk_report/wizard/dingtalk_report_list.py
+++ dingtalk_report/wizard/dingtalk_report_list.py
@@ -98,7 +98,8 @@ class DingTalkReportListTran(models.TransientModel):
                                 [('report_id', '=', data.get('report_id'))])
                             if not reports:
                                 self.env['dingtalk.report.report'].create(report_data)
-                            self.env['dingtalk.report.report'].update(report_data)
+                            # 已存在日志，更新日志
+                            reports.sudo().write(report_data)
                         # 是否还有下一页
                         if result.get('has_more'):
                             cursor = result.get('next_cursor')
